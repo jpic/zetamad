ae9e3405ac6fd36ef9b826742c44e40961ade7c1
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-03-22 14:54:31 +0000

    Added structure for madModel sql
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@843 68bcc517-35a1-de11-95e8-003048d8a48a

---
+++ mad/model/structure.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `mad_model`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `mad_model` (
+      `id_attribute` int(15) default NULL,
+      `id` binary(16) default NULL,
+      `attribute_key` varchar(50) NOT NULL,
+      `attribute_value` text
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
