db1a6678f6bb38f6bf14945b09f71ff89b419e9b
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-03-23 16:48:31 +0000

    Added basic form support in core
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@858 68bcc517-35a1-de11-95e8-003048d8a48a

--- mad/model/controller.php
+++ mad/model/controller.php
@@ -30,21 +30,49 @@ class madModelController extends ezcMvcController {
 
     public function doForm(  ) {
         $result = new ezcMvcResult(  );
+        // get the form configuration
+        $formName = $this->request->variables['form'];
+        $formConfiguration = $this->registry->configuration->settings['forms'][$formName];
+        $formNamespaceFieldValue = $formConfiguration['fields']['namespace']['value'];
 
+        // make/get the object
         $object = new madBase();
         if ( isset( $this->id ) ) {
             $object['id'] = $this->id;
             $this->registry->model->refresh( $object );
         }
 
+        // add namespace if required
         if ( !isset( $object['namespace'] ) ) {
-            $object['namespace'] = 'recipe';
+            $object['namespace'] = $formNamespaceFieldValue;
+        } else { // check the namespace
+            if ( $object['namespace'] != $formNamespaceFieldValue ) {
+                throw new Exception(
+                     "The form namespace field value '{$formNamespaceFieldValue}' is different from the object namespace {$object['namespace']}!"
+                );
+            }
         }
 
+        // add the object to result variables for reuse in the template
         $result->variables['object'] = $object;
+        // add the form configuration to result variables
+        $result->variables['formConfiguration'] = $formConfiguration;
 
+        // prepare whatever the form needs to render
+        foreach( $formConfiguration['fields'] as $name => $field ) {
+            if ( $field['widget'] == 'autocomplete' ) {
+                // fill $field['choices'] with existing values
+                $field['choices'] = $this->registry->model->getAttributeValues( $name );
+            }
+        }
+
+        // save the object
         if ( $this->request->protocol == 'http-post' ) {
-            $object['title'] = $_POST['title'];
+            foreach( $formConfiguration['fields'] as $name => $field ) {
+                if ( $_POST[$name] ) {
+                    $object[$name] = $_POST[$name];
+                }
+            }
             $this->registry->model->save( $object );
         }
 
--- mad/model/templates/form.php
+++ mad/model/templates/form.php
@@ -1,5 +1,54 @@
-<?php
+<link rel="stylesheet" type="text/css" href="<?php echo $this->getAbsoluteStaticUrl( 'css/uni-form.css' ) ; ?>" />
+<script type="text/javascript" src="<?php echo $this->getAbsoluteStaticUrl( 'js/uni-form.jquery.js' ) ; ?>"></script>
 
-echo 'FOO';
+<form action="" method="post" class="uniForm">
+    <fieldset class="inlineLabels">
+    <?php foreach( $this->formConfiguration['fields'] as $name => $field ): ?>
+        <?php
+        // skip fields with hard coded value (ie. namespace)
+        if ( isset( $field['value'] ) ) continue;
+        ?>
+    
+        <div class="ctrlHolder">
+            <label for="<?php echo $name; ?>"><?php if ( isset( $field['required'] ) ): ?><em>*</em> <?php endif ?><?php echo ucfirst( $field['label'] ); ?></label>
 
-?>
+            <?php if ( !isset( $field['widget'] ) ): ?>
+            <input name="<?php echo $name; ?>" id="<?php echo $name; ?>" type="text" class="textInput" />
+
+            <?php elseif ( $field['widget'] == 'textarea' ): ?>
+            <textarea name="<?php echo $name; ?>" id="<?php echo $name; ?>"></textarea>
+            
+            <?php elseif ( $field['widget'] == 'autocomplete' ): ?>
+            <input name="<?php echo $name; ?>" id="<?php echo $name; ?>" type="text" class="textInput" />
+                
+                <?php if ( $field['choices'] ): ?>
+                <script type="text/javascript">
+                $(document).ready( function() {
+                    // try not to mess with other input fields in the page
+                    $('form.uniForm input[name=<?php echo $name; ?>]').autocomplete([
+                    "<?php echo join( '", "', $field['choices'] ) ?>"
+                    ]);
+                });
+                </script>
+                <?php endif ?>
+
+            <?php endif ?>
+
+            <?php if ( isset( $field['help'] ) ): ?>
+            <p class="formHint"><?php echo ucfirst( $field['help'] ); ?></p>
+            <?php endif ?>
+        </div>
+
+    <?php endforeach ?>
+    </fieldset>
+
+    <div class="buttonHolder">
+      <button type="reset" class="resetButton">Rétablir les valeurs initiales</button>
+      <button type="submit" class="primaryAction">Enregistrer</button>
+    </div>
+
+</form>
+
+<script type="text/javascript">
+$( 'form.uniForm input:first').focus();
+</script>
