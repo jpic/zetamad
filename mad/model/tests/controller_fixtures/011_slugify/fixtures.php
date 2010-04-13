f9dadbf857c19a453e383bcd87ae0338af6cc5e7
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-04-13 02:24:14 +0000

    Fixed slugify tests
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1005 68bcc517-35a1-de11-95e8-003048d8a48a

---
+++ mad/model/tests/controller_fixtures/011_slugify/fixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+$options = array (
+    'label' => 'save a recipe',
+    'fields' => array (
+        'namespace' =>  array (
+            'value' => 'recipe',
+        ),
+        'title' =>  array (
+            'label'    => 'recipe title',
+            'required' => true,
+        ),
+        'slug' => array( 
+            'slugify' => 'title'
+        ),
+    ),
+);
+
+$request                               = new ezcMvcRequest(  );
+$request->protocol                     = 'http-post';
+$request->variables['form']            = 'recipe';
+$request->variables['successRoute']    = 'recipe.details';
+$request->variables['recipe']          = array(  );
+$request->variables['recipe']['title'] = 'TiPonch\'ôéûà';
+
+$form = new madBase( array( 
+    'title'     => 'TiPonch\'ôéûà',
+    'namespace' => 'recipe',
+    'id'        => '1FDB6574-3E40-11DF-8BB4-1B969F5BE980',
+    'slug'      => 'tiponch-oeua',
+) );
+$form->name = 'recipe';
+
+return array( $options, $request, $form );
+?>
