63c981ac4e4ff1b4095f19073a48778e2f6fb2b9
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-04-20 16:14:09 +0000

    Added DBAL
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1044 68bcc517-35a1-de11-95e8-003048d8a48a

--- mad/model/query.php
+++ mad/model/query.php
@@ -1,6 +1,6 @@
 <?php
 
-class madQuery {
+class madBaseQuery implements Iterator {
     public $type = '';
     public $select = array(  );
     public $from = array(  );
@@ -9,6 +9,15 @@ class madQuery {
     public $count = null;
     public $sql = '';
     public $comma = "\n\t, ";
+    public $pdo = null;
+    public $rows = array(  );
+    public $argument = array(  );
+    public $executed = false;
+    public $fetchMode = PDO::FETCH_ASSOC;
+
+    public function __construct( PDO $pdo ) {
+        $this->pdo = $pdo;
+    }
 
     public function select( $column, $alias = null ) {
         $query = clone $this;
@@ -47,29 +56,45 @@ class madQuery {
     }
 
     public function escapeReference( $argument ) {
-        if( preg_match( '/^[a-zA-Z_]+$/', $argument ) ) {
+        if( $argument instanceof madQuery && $argument->type == 'SELECT') {
+            // subquery
+            return "( " . $argument->getSql(  ) . " )";
+        } elseif( preg_match( '/^[a-zA-Z_]+$/', $argument ) ) {
             // quote standard sql reference
             return "`$argument`"; 
         } elseif( preg_match( '/^([a-zA-Z_]+).([a-zA-Z_]+)$/', $argument, $matches, PREG_OFFSET_CAPTURE ) ) {
             // quote standard sql reference
             return "`{$matches[1][0]}`.`{$matches[2][0]}`"; 
-        } elseif( $argument instanceof madQuery && $argument->type == 'SELECT') {
-            // subquery
-            return "( " . $argument->getSql(  ) . " )";
         } else {
             return $argument;
         }
     }
 
+    public function showTables(  ) {
+        $query = clone $this;
+        switch( $query->pdo->getAttribute( PDO::ATTR_DRIVER_NAME ) ) {
+            case 'sqlite':
+                $query->sql = 'SELECT name FROM sqlite_master WHERE type="table"';
+                break;
+            case 'mysql':
+                $query->sql = 'SHOW TABLES';
+                $query->fetchMode = PDO::FETCH_NUM;
+                break;
+            default:
+                trigger_error( "What pdo driver are you using?" );
+        }
+        return $query;
+    }
+
     public function factory(  ) {
-        $query = new madQuery(  );
+        $query = new madQuery( $this->pdo );
         $query->comma = $this->comma;
         return $query;
     }
 
     public function getSql(  ) {
         if ( $this->sql ) {
-            return $sql;
+            return $this->sql;
         }
 
         $result = array( $this->type );
@@ -133,7 +158,64 @@ class madQuery {
     }
 
     public function __clone(  ) {
-        $this->sql = false;
+        $this->sql      = false;
+        $this->executed = false;
+        $this->rows     = array(  );
+    }
+
+    public function execute(  ) {
+        $statement = $this->pdo->prepare( $this->getSql(  ) );
+        
+        if ( !$statement ) {
+            trigger_error( "query failed " . $this->getSql(  ) );
+            return false;
+        }
+
+        $statement->setFetchMode( $this->fetchMode );
+        $statement->execute();
+
+        $this->rows = $statement->fetchAll(  );
+        $this->executed = true;
+    }
+
+    public function rewind() {
+        if ( !$this->executed ) {
+            $this->execute(  );
+        }
+
+        return reset($this->rows);
+    }
+
+    public function current() {
+        return current($this->rows);
+    }
+
+    public function key() {
+        return key($this->rows);
+    }
+
+    public function next() {
+        return next($this->rows);
+    }
+
+    public function valid() {
+        return key($this->rows) !== null;
+    }
+}
+
+class madQuery extends madBaseQuery {
+    public $configuration = null;
+
+    public function __construct( PDO $db, $configuration = null ) {
+        parent::__construct( $db );
+        $this->configuration = $configuration;   
+    }
+
+    public function createIndexTables(  ) {
+        // check if index table exists
+        foreach( $this->showTables(  ) as $table ) {
+            var_dump( $table );
+        }
     }
 }
 
--- mad/model/tests/query.php
+++ mad/model/tests/query.php
@@ -1,13 +1,29 @@
 <?php
 
-class madQueryTest extends PHPUnit_Framework_TestCase {
+class madBaseQueryTest extends PHPUnit_Framework_TestCase {
     public static function suite(  ) {
         return new PHPUnit_Framework_TestSuite( __CLASS__ );
     }
 
+    public $query = null;
+    public function setUp(  ) {
+        $registry = madRegistry::instance();
+        $registry->router = $this->getMock( 'madCoreRouter', array(  ), array( new ezcMvcrequest(  ), array(  ) ));
+        $registry->router->expects( $this->any(  ) )
+                         ->method( 'generateUrl' )
+                         ->will( $this->returnValue( '/bar' ) );
+
+        $this->db = $registry->database;
+        $registry->model = new madModelMock( $this->db );
+
+        $this->model = $registry->model;
+        $this->db->query( 'truncate mad_model' );
+        $this->query = new madBaseQuery( $this->db );
+        $this->query->comma = ', ';
+    }
+
     public function testSelect(  ) {
-        $query = new madQuery(  );
-        $query->comma = ', ';
+        $query = clone $this->query;
 
         $query = $query->select( 'foo' );
         $this->assertEquals( 
@@ -17,9 +33,8 @@ class madQueryTest extends PHPUnit_Framework_TestCase {
     }
     
     public function testSelectAlias(  ) {
-        $query = new madQuery(  );
-        $query->comma = ', ';
-        
+        $query = clone $this->query;
+
         $query = $query->select( 'bar', 'barAlias' );
         $this->assertEquals( 
             'SELECT `bar` AS `barAlias`',
@@ -28,9 +43,8 @@ class madQueryTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testSelectMixed(  ) {
-        $query = new madQuery(  );
-        $query->comma = ', ';
-        
+        $query = clone $this->query;
+
         $query = $query->select( 'foo' )->select( 'bar', 'barAlias' );
         $this->assertEquals( 
             'SELECT `foo`, `bar` AS `barAlias`',
@@ -39,8 +53,7 @@ class madQueryTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testFrom(  ) {
-        $query = new madQuery(  );
-        $query->comma = ', ';
+        $query = clone $this->query;
         
         $query = $query->select( 'foo' )->from( 'bar' );
 
@@ -51,8 +64,7 @@ class madQueryTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testFromAlias(  ) {
-        $query = new madQuery(  );
-        $query->comma = ', ';
+        $query = clone $this->query;
         $query = $query->select( 'foo' );
 
         $query = $query->from( 'bar', 'barAlias' );
@@ -63,8 +75,7 @@ class madQueryTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testFromSubselect(  ) {
-        $query = new madQuery(  );
-        $query->comma = ', ';
+        $query = clone $this->query;
         $query = $query->select( 'foo_T.foo', 'foo' )
                        ->from( $query->factory(  )
                                      ->select( 'foo' )
@@ -79,8 +90,7 @@ class madQueryTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testFromMixed(  ) {
-        $query = new madQuery(  );
-        $query->comma = ', ';
+        $query = clone $this->query;
         $query = $query->select( 'foo_T.foo', 'foo' )
                        ->from( $query->factory(  )
                                      ->select( 'foo' )
@@ -97,8 +107,7 @@ class madQueryTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testOrder(  ) {
-        $query = new madQuery(  );
-        $query->comma = ', ';
+        $query = clone $this->query;
         $query = $query->select( '*' )->from( 'bar' )->order( 'id' );
         $this->assertEquals( 
             "SELECT * FROM `bar` ORDER BY `id` ASC",
@@ -107,8 +116,8 @@ class madQueryTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testOrderDesc(  ) {
-        $query = new madQuery(  );
-        $query->comma = ', ';
+        $query = clone $this->query;
+
         $query = $query->select( '*' )->from( 'bar' )->order( 'id', 'desc' );
         $this->assertEquals( 
             "SELECT * FROM `bar` ORDER BY `id` DESC",
@@ -117,13 +126,55 @@ class madQueryTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testLimit(  ) {
-        $query = new madQuery(  );
-        $query->comma = ', ';
+        $query = clone $this->query;
         $query = $query->select( '*' )->from( 'bar' )->limit( 0, 100 );
         $this->assertEquals( 
             "SELECT * FROM `bar` LIMIT 0, 100",
             $query->getSql(  )
         );
+    }
+
+    //public function testShowTables(  ) {
+        //$model = new madModel( $this->query->pdo );
+        //$model->createTable(  );
+
+        //$query = $this->query->showTables(  );
+        //$query->execute(  );
+        //var_dump( $query->rows, $query->executed );
+    //}
+}
+
+class madQueryTest extends PHPUnit_Framework_TestCase {
+    static public function suite(  ) {
+        return new PHPUnit_Framework_TestSuite( __CLASS__ );
+    }
+
+    public $query = null;
+    
+    public function __construct(  ) {
+
+    }
+
+    public function setUp(  ) {
+        $registry = madRegistry::instance();
+        $registry->router = $this->getMock( 'madCoreRouter', array(  ), array( new ezcMvcrequest(  ), array(  ) ));
+        $registry->router->expects( $this->any(  ) )
+                         ->method( 'generateUrl' )
+                         ->will( $this->returnValue( '/bar' ) );
+
+        $this->db = $registry->database;
+        $registry->model = new madModelMock( $this->db );
+        $this->model = $registry->model;
+
+
+        $query = new madBaseQuery( $this->db );
+        $query = $query->showTables(  );
+        foreach(  )
+        $this->db->query( 'truncate mad_model' );
+    }
+
+    public function testCreateIndex(  ) {
+        #$query = $this->query->createIndexTables(  );
     }
 }
 
