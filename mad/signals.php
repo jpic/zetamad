b704467999910142b5adc7fb057104c877e696d5
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-04-26 17:24:34 +0000

    Documented/solidified madSignals
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1103 68bcc517-35a1-de11-95e8-003048d8a48a

--- mad/signals.php
+++ mad/signals.php
@@ -1,9 +1,52 @@
 <?php
-
+/**
+ * File containing the madSignals class.
+ * 
+ * @package Mad
+ * @version //autogen//
+ * @copyright Copyright (C) 2010-2011 madeleinemarket.com. All rights reserved.
+ * @author James Pic <jamespic@gmail.com> 
+ * @license http://madeleinemarket.com/code/license
+ */
+/**
+ * madSignals is an implementation of the Signal/Slot design pattern.
+ *
+ * Usage:
+ * <code>
+ * $signals = new madSignals();
+ *
+ * function fooBar( $arg ) {
+ *     echo $arg;
+ * }
+ *
+ * $signals->connect( 'foo', 'fooBar' );
+ *
+ * $signals->send( 'foo', array( 'test' ) ); // will echo 'test'
+ * </code>
+ * 
+ * @package Mad
+ * @author James Pic <jamespic@gmail.com> 
+ */
 class madSignals {
+    /**
+     * List of callbacks connected to a signal.
+     * 
+     * @var array(string=>callback)
+     */
     protected $signals;
 
+    /**
+     * Connect a signal with a callback.
+     * 
+     * @param string $signal 
+     * @param mixed $callback 
+     * @return void
+     */
     public function connect( $signal, $callback ) {
+        if ( !is_string( $signal ) ) {
+            trigger_error( "\$signal is not a string" );
+        }
+
         if ( !isset( $this->signals[$signal] ) ) {
             $this->signals[$signal] = array(  );
         }
@@ -11,6 +54,14 @@ class madSignals {
         $this->signals[$signal][] = $callback;
     }
     
+    /**
+     * Send a signal with arguments, will execute all connected callbacks with 
+     * the given arguments list.
+     * 
+     * @param mixed $signal 
+     * @param array $arguments 
+     * @return false if a connected callback returned false.
+     */
     public function send( $signal, array $arguments = array(  ) ) {
         if ( !isset( $this->signals[$signal] ) ) {
             return true;
@@ -20,9 +71,17 @@ class madSignals {
             return true;
         }
 
+        $return = true;
+
         foreach( $this->signals[$signal] as $callback ) {
-            call_user_func_array( $callback, $arguments );
+            $result = call_user_func_array( $callback, $arguments );
+
+            if ( !$result ) {
+                $return = false;
+            }
         }
+
+        return $result;
     }
 }
 
