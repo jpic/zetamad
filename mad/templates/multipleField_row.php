5caad63dba1118ed6c3b6be081dc77bb58de8704
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-04-27 12:29:11 +0000

    Fixed string conversion in multiple field template
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1111 68bcc517-35a1-de11-95e8-003048d8a48a

--- mad/templates/multipleField_row.php
+++ mad/templates/multipleField_row.php
@@ -1,10 +1,9 @@
 <?php
 $inputName = $this->form->name . '[' . $multipleFieldName . '][' . $count . ']';
-$deleteName = $this->form->name . '[' . $multipleFieldName . '.DELETE][]';
 ?>
 <tr>
     <td class="actualValue">
-        <input type="text" value="<?php echo htmlentities( $multipleFieldValue ) ?>" name="<?php echo $inputName ?>" class="textInput" />
+        <input type="text" value="<?php $this->e( $multipleFieldValue ) ?>" name="<?php echo $inputName ?>" class="textInput" />
     </td>
     <td class="deleteField">
         <input class="deleteRow" type="button" value="Effacer" <?php if ( $count == 0 ): ?>disabled="disabled"<?php endif ?>  />
@@ -12,5 +11,4 @@ $deleteName = $this->form->name . '[' . $multipleFieldName . '.DELETE][]';
 </tr>
 <?php
 unset( $inputName );
-unset( $deleteName );
 ?>
