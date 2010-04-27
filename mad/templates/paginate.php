8c8a2971d8ec42e558baf40b6d945532521d2478
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-04-27 12:29:12 +0000

    Quick hack to make the pagination urls to work
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1114 68bcc517-35a1-de11-95e8-003048d8a48a

--- mad/templates/paginate.php
+++ mad/templates/paginate.php
@@ -1,17 +1,17 @@
 <?php if ( $this->currentPage == 1 ): ?>
 Premier Précedent
 <?php else: ?>
-<a href="<?php echo $this->request->uri ?>?page=1">Premier</a>
-<a href="<?php echo $this->request->uri ?>?page=<?php $this->e( $this->currentPage - 1 ) ?>">Précédent</a>
+<a href="<?php echo $_SERVER['REQUEST_URI'] ?>?page=1">Premier</a>
+<a href="<?php echo $_SERVER['REQUEST_URI'] ?>?page=<?php $this->e( $this->currentPage - 1 ) ?>">Précédent</a>
 <?php endif ?>
 
 <?php for( $i=1; $i <= $this->lastPage; $i++ ): ?>
-<a href="<?php echo $this->request->uri ?>?page=<?php $this->e( $i ) ?>"><?php $this->e( $i ) ?></a> 
+<a href="<?php echo $_SERVER['REQUEST_URI'] ?>?page=<?php $this->e( $i ) ?>"><?php $this->e( $i ) ?></a> 
 <?php endfor ?>
 
 <?php if ( $this->currentPage == $this->lastPage ): ?>
 Suivant Dernier
 <?php else: ?>
-<a href="<?php echo $this->request->uri ?>?page=<?php $this->e( $this->currentPage + 1 ) ?>">Suivant</a>
-<a href="<?php echo $this->request->uri ?>?page=<?php $this->e( $this->lastPage ) ?>">Dernier</a>
+<a href="<?php echo $_SERVER['REQUEST_URI'] ?>?page=<?php $this->e( $this->currentPage + 1 ) ?>">Suivant</a>
+<a href="<?php echo $_SERVER['REQUEST_URI'] ?>?page=<?php $this->e( $this->lastPage ) ?>">Dernier</a>
 <?php endif ?>
