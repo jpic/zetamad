67968991d6f596bd8bd125a5ae35694efeea916b
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-05-03 09:36:14 +0000

    Added view method to check if a user has a role
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1183 68bcc517-35a1-de11-95e8-003048d8a48a

--- mad/contrib/authentication/view_plugin.php
+++ mad/contrib/authentication/view_plugin.php
@@ -2,8 +2,15 @@
 
 class madAuthenticationViewHandlerPlugin {
     public function hasRole( $role ) {
-        var_dump( $role );
-        return 'bar';
+        if ( !isset( $this->request->variables['user'] ) ) {
+            return false;
+        }
+
+        if ( !isset( $this->request->variables['user']['role'] ) ) {
+            return false;
+        }
+
+        return $this->request->variables['user']['role'] == $role;
     }
 }
 
