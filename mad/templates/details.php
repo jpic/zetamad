d00a40357447534ed17cd9b17aae6d218ff8e58c
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-05-03 09:27:28 +0000

    Made the default details template configurable and prettier
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1145 68bcc517-35a1-de11-95e8-003048d8a48a

--- mad/templates/details.php
+++ mad/templates/details.php
@@ -1,9 +1,22 @@
+<?php if ( isset( $this->configuration['title'] ) ): ?>
+<h1><?php $this->e( ucfirst( $this->configuration['title'] ) ) ?></h1>
+<?php else: ?>
 <h1>Page de details d'object par défaut</h1>
 
+<p>
 Pour surcharger cette page, créer un template du nom de <?php echo $this->object['namespace'] ?>_details.php et utiliser $this->object dedans.
+</p>
 
-<?php
+<p>
+Pour changer ce titre et cacher le message ci-dessus, parametrer la variable de route "title".
+</p>
+<?php endif ?>
 
-var_dump( $this->object );
-
-?>
+<table>
+<?php foreach( $this->object as $key => $value ): ?>
+    <tr>
+        <th><?php $this->e( $key ) ?></th>
+        <td><?php $this->e( $value ) ?></td>
+    </tr>
+<?php endforeach ?>
+</table>
