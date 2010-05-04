2decdc2e8276ad6c8127cc2ce0c63c612cadff96
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-05-04 08:15:09 +0000

    Implemented template functions isAuthenticated and loginUrl in the authentication view plugin
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1209 68bcc517-35a1-de11-95e8-003048d8a48a

--- mad/contrib/authentication/view_plugin.php
+++ mad/contrib/authentication/view_plugin.php
@@ -12,6 +12,18 @@ class madAuthenticationViewHandlerPlugin {
 
         return $this->request->variables['user']['role'] == $role;
     }
+
+    public function isAuthenticated(  ) {
+        if ( isset( $this->request->variables['user'] ) ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function loginUrl(  ) {
+        return madRegistry::instance()->configuration->getSetting( 'applications', 'authentication', 'loginUrl' );
+    }
 }
 
 ?>
