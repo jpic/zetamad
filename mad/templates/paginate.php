e8db8c4b165f0822b8cf1c0f06211b8ef2c2e837
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-05-06 12:28:02 +0000

    Designed pagination, by Laurent
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1266 68bcc517-35a1-de11-95e8-003048d8a48a

--- mad/templates/paginate.php
+++ mad/templates/paginate.php
@@ -1,17 +1,23 @@
-<?php if ( $this->currentPage == 1 ): ?>
-Premier Précedent
-<?php else: ?>
-<a href="<?php echo $this->request->uri ?>?page=1">Premier</a>
-<a href="<?php echo $this->request->uri ?>?page=<?php $this->e( $this->currentPage - 1 ) ?>">Précédent</a>
-<?php endif ?>
-
-<?php for( $i=1; $i <= $this->lastPage; $i++ ): ?>
-<a href="<?php echo $this->request->uri ?>?page=<?php $this->e( $i ) ?>"><?php $this->e( $i ) ?></a> 
-<?php endfor ?>
-
-<?php if ( $this->currentPage == $this->lastPage ): ?>
-Suivant Dernier
-<?php else: ?>
-<a href="<?php echo $this->request->uri ?>?page=<?php $this->e( $this->currentPage + 1 ) ?>">Suivant</a>
-<a href="<?php echo $this->request->uri ?>?page=<?php $this->e( $this->lastPage ) ?>">Dernier</a>
-<?php endif ?>
+<table class="pagination" align="center">
+    <tbody>
+        <tr>
+        <?php if ( $this->currentPage == 1 ): ?>
+        Premier Précedent
+        <?php else: ?>
+        <td class="pagination_previous"><a href="<?php echo $this->request->uri ?>?page=1">Premier</a>
+        <td class="pagination_previous"><a href="<?php echo $this->request->uri ?>?page=<?php $this->e( $this->currentPage - 1 ) ?>">Précédent</a></td>
+        <?php endif ?>
+        
+        <?php for( $i=1; $i <= $this->lastPage; $i++ ): ?>
+        <td class="pagenumber"><a href="<?php echo $this->request->uri ?>?page=<?php $this->e( $i ) ?>"><?php $this->e( $i ) ?></a></td>
+        <?php endfor ?>
+        
+        <?php if ( $this->currentPage == $this->lastPage ): ?>
+        Suivant Dernier
+        <?php else: ?>
+        <td class="pagination_next"><a href="<?php echo $this->request->uri ?>?page=<?php $this->e( $this->currentPage + 1 ) ?>">Suivant</a></td>
+        <td class="pagination_next"><a href="<?php echo $this->request->uri ?>?page=<?php $this->e( $this->lastPage ) ?>">Dernier</a></td>
+        <?php endif ?>
+        </tr>
+    </tbody>
+</table>
