20cde5ce888d5074eff824161d301767fa83f565
laurentTouchet <laurentTouchet@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-05-07 09:53:21 +0000

    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1282 68bcc517-35a1-de11-95e8-003048d8a48a

--- mad/templates/details.php
+++ mad/templates/details.php
@@ -1,22 +1,26 @@
+<style>
+/* Redondance du h2.title, possibilité de le mettre dans le global pour ne pas l'appeler à chaque page ? */
+h2.title { float: left; border:medium none; color: #000000; font-family:'Times','Helvetica',serif; font-size:35px; font-weight:100; line-height:40px; margin: 15px 0 15px 0; }
+.users_table { padding: 10px; font: normal 11px Verdana; }
+.users_table .label { background: #f7f7f7; width: 250px; }
+</style>
 <?php if ( isset( $this->configuration['title'] ) ): ?>
-<h1><?php $this->e( ucfirst( $this->configuration['title'] ) ) ?></h1>
+<h2 class="title"><?php $this->e( ucfirst( $this->configuration['title'] ) ) ?></h2>
 <?php else: ?>
-<h1>Page de details d'object par défaut</h1>
-
+<h2 class="title">D&eacute;tails indisponibles</h2>
 <p>
-Pour surcharger cette page, créer un template du nom de <?php echo $this->object['namespace'] ?>_details.php et utiliser $this->object dedans.
+    Pour surcharger cette page, créer un template du nom de <?php echo $this->object['namespace'] ?>_details.php et utiliser $this->object dedans.
 </p>
-
 <p>
-Pour changer ce titre et cacher le message ci-dessus, parametrer la variable de route "title".
+    Pour changer ce titre et cacher le message ci-dessus, parametrer la variable de route "title".
 </p>
 <?php endif ?>
 
-<table>
+<table class="users_table">
 <?php foreach( $this->object as $key => $value ): ?>
     <tr>
-        <th><?php $this->e( $key ) ?></th>
-        <td><?php $this->e( $value ) ?></td>
+        <th class="label"><?php $this->e( ucfirst( $key ) ) ?></th>
+        <td class="object"><?php $this->e( $value ) ?></td>
     </tr>
 <?php endforeach ?>
 </table>
