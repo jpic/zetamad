50334a9bb0c8247c396a23e264896af8d1fc04a8
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-05-07 09:58:59 +0000

    Allow to configure label and fields in the default details template
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1283 68bcc517-35a1-de11-95e8-003048d8a48a

--- mad/templates/details.php
+++ mad/templates/details.php
@@ -1,8 +1,8 @@
 <style>
 /* Redondance du h2.title, possibilité de le mettre dans le global pour ne pas l'appeler à chaque page ? */
 h2.title { float: left; border:medium none; color: #000000; font-family:'Times','Helvetica',serif; font-size:35px; font-weight:100; line-height:40px; margin: 15px 0 15px 0; }
-.users_table { padding: 10px; font: normal 11px Verdana; }
-.users_table .label { background: #f7f7f7; width: 250px; }
+.object_table { padding: 10px; font: normal 11px Verdana; }
+.object_table .label { background: #f7f7f7; width: 250px; }
 </style>
 <?php if ( isset( $this->configuration['title'] ) ): ?>
 <h2 class="title"><?php $this->e( ucfirst( $this->configuration['title'] ) ) ?></h2>
@@ -16,11 +16,22 @@ h2.title { float: left; border:medium none; color: #000000; font-family:'Times',
 </p>
 <?php endif ?>
 
-<table class="users_table">
-<?php foreach( $this->object as $key => $value ): ?>
-    <tr>
-        <th class="label"><?php $this->e( ucfirst( $key ) ) ?></th>
-        <td class="object"><?php $this->e( $value ) ?></td>
-    </tr>
-<?php endforeach ?>
+<table class="object_table">
+<? if ( isset( $this->configuration['fields'] ) ): ?>
+    <?php foreach( $this->configuration['fields'] as $key => $label ): ?>
+        <?php if ( !isset( $this->object[$key] ) ) continue; ?>
+
+        <tr>
+            <th class="label"><?php $this->e( ucfirst( $label ) ) ?></th>
+            <td class="value"><?php $this->e( $this->object[$key] ) ?></td>
+        </tr>
+    <?php endforeach ?>
+<?php else: ?>
+    <?php foreach( $this->object as $key => $value ): ?>
+        <tr>
+            <th class="label"><?php $this->e( ucfirst( $key ) ) ?></th>
+            <td class="value"><?php $this->e( $value ) ?></td>
+        </tr>
+    <?php endforeach ?>
+<?php endif ?>
 </table>
