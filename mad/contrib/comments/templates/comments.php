60bbd18e9f6d8a4d5212c5d4fda52b503bac150e
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-05-10 09:02:29 +0000

    Fugbix comments template
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1314 68bcc517-35a1-de11-95e8-003048d8a48a

--- mad/contrib/comments/templates/comments.php
+++ mad/contrib/comments/templates/comments.php
@@ -1,8 +1,6 @@
 <style>
 .button { cursor: pointer; }
 </style>
-<?php if ( count( $this->object->commentSet ) ): ?>
-
 <div class="product-tab">
     <div style="clear: left; width: 735px; overflow: auto; margin: 15px 0pt;">
         <div class="title-tab" style="padding-bottom: 5px; margin-bottom: 0px;">Commentaires clients</div>
@@ -10,13 +8,12 @@
         <p>Aucun commentaire n'a &eacute;t&eacute; publi&eacute; pour le moment.</p>
         <?php else: ?>
             <?php foreach( $this->object->commentSet as $comment ): ?>
-                <?php if ( !$comment['moderated'] ) continue /* skip non moderated comments */ ?>
             <p><?php $this->e( $comment['comment'] ) ?></p>
             <p><?php $this->e( $comment['user']['firstName'] . ' ' . $comment['user']['lastName'] ) ?></p>
             <?php endforeach ?>
         <?php endif ?>
         
-        <?php if ( $this->isAuthenticated(  ) ): ?>
+        <?php if ( $this->isAuthenticated ): ?>
         <form action="<?php echo $this->url( 'comments.post', array( 'id' => $this->object ) ) ?>" method="post">
             <textarea style="font: 12px Verdana; width: 400px; height: 80px;" name="comment" class="input_create"></textarea>
             <input type="hidden" name="object" value="<?php $this->e( $this->object['id'] ) ?>" />
@@ -26,7 +23,7 @@
         </form>
         <?php else: ?>
         <div id="idTab5">
-            <p><a href="<?php echo $this->loginUrl(  ) ?>">Identifiez-vous ou cr&eacute;ez votre compte pour laisser un commentaire.</a></p>
+            <p><a href="<?php echo $this->loginUrl ?>">Identifiez-vous ou cr&eacute;ez votre compte pour laisser un commentaire.</a></p>
         </div>
         <?php endif ?>
     </div>
