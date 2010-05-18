aad0753f7c21da1220af47173dede434ee8ecf58
laurentTouchet <laurentTouchet@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-05-18 12:13:44 +0000

    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1402 68bcc517-35a1-de11-95e8-003048d8a48a

--- mad/contrib/comments/templates/comments.php
+++ mad/contrib/comments/templates/comments.php
@@ -7,14 +7,19 @@
         <?php if ( !count( $this->object->commentSet ) ): ?>
         <p>Aucun commentaire n'a &eacute;t&eacute; publi&eacute; pour le moment.</p>
         <?php else: ?>
-            <?php foreach( $this->object->commentSet as $comment ): ?>
-            <p><?php $this->e( $comment['comment'] ) ?></p>
-            <?php if ( isset( $comment['user'] ) && $comment['user'] instanceof madObject ) : ?>
-            <p><?php $this->e( $comment['user']['firstName'] . ' ' . $comment['user']['lastName'] ) ?></p>
+        <?php foreach( $this->object->commentSet as $comment ): ?>
+            <div style="float: left; padding: 10px 0pt 0pt;">
+                    <?php if ( isset( $comment['user'] ) && $comment['user'] instanceof madObject ) : ?>
+                    <span style="font: 12px Verdana; color: rgb(132, 132, 132);">Par &nbsp;<?php $this->e( $comment['user']['firstName'] . ' ' . $comment['user']['lastName'] ) ?></span>
+            </div>
+            <h5 style="font-weight: normal;">
+                <p style="clear: both; font-size: 12px; margin: 0px; line-height: 20px; color: rgb(102, 102, 102);">
+                <?php $this->e( $comment['comment'] ) ?>
+                </p>
+            </h5>
             <?php endif ?>
             <?php endforeach ?>
         <?php endif ?>
-        
         <?php if ( $this->isAuthenticated ): ?>
         <form action="<?php echo $this->url( 'comments.post', array( 'id' => $this->object ) ) ?>" method="post">
             <textarea style="font: 12px Verdana; width: 400px; height: 80px;" name="comment" class="input_create"></textarea>
