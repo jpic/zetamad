13c1a73973d05e5325d2aeeb6352a986e8c62c07
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-06-09 13:41:48 +0000

    Also fetch id of an authenticated user, as that will make the difference for isAuthenticated variable
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1574 68bcc517-35a1-de11-95e8-003048d8a48a

--- mad/contrib/prestashop/bootstrap.php
+++ mad/contrib/prestashop/bootstrap.php
@@ -6,9 +6,9 @@ function prestashopAuthentication( ezcMvcRequest $request, ezcMvcRouter $router 
     
     if ( $cookie->isLogged(  ) ) {
         $users = madFramework::query(
-            'select firstName, lastName, email, prestashopId from user where prestashopId = :prestashopId',
+            'select id, firstName, lastName, email, prestashopId from user where prestashopId = :prestashopId',
             array(
-                'prestashopId' => intval( $cookie->id_customer )
+                'prestashopId' => (string)intval( $cookie->id_customer )
             )
         );
         
