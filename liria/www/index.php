99d3bf51165f45a8bc82b459f8df54e5925d323a
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-06-09 13:41:36 +0000

    Remove benchmark code
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1570 68bcc517-35a1-de11-95e8-003048d8a48a

--- liria/www/index.php
+++ liria/www/index.php
@@ -1,10 +1,6 @@
 <?php
-$start = microtime(true);
-
 include '../entry_bootstrap.php';
 
 $framework->dispatcher->run(  );
-$end = microtime(true);
-$total = $end - $start;
-shell_exec( "echo $start - $end = $total > /tmp/bench" );
+
 ?>
