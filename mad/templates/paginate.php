55221b32fca007e4016ce551d60bfe0f85c03bb0
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-06-11 08:54:54 +0000

    Do not show pagination if only one page
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1601 68bcc517-35a1-de11-95e8-003048d8a48a

--- mad/templates/paginate.php
+++ mad/templates/paginate.php
@@ -1,21 +1,25 @@
-<table class="pagination" align="center">
-    <tbody>
-        <tr>
-        <?php if ( $this->currentPage == 1 ): ?>
-        <?php else: ?>
-        <td class="pagination_previous"><a href="<?php echo $this->request->variables['prefixedUrl'] ?>?page=1">Premier</a>
-        <td class="pagination_previous"><a href="<?php echo $this->request->variables['prefixedUrl'] ?>?page=<?php $this->e( $this->currentPage - 1 ) ?>">Précédent</a></td>
-        <?php endif ?>
-        
-        <?php for( $i=1; $i <= $this->lastPage; $i++ ): ?>
-        <td class="pagenumber"><a href="<?php echo $this->request->variables['prefixedUrl'] ?>?page=<?php $this->e( $i ) ?>"><?php $this->e( $i ) ?></a></td>
-        <?php endfor ?>
-        
-        <?php if ( $this->currentPage == $this->lastPage ): ?>
-        <?php else: ?>
-        <td class="pagination_next"><a href="<?php echo $this->request->variables['prefixedUrl'] ?>?page=<?php $this->e( $this->currentPage + 1 ) ?>">Suivant</a></td>
-        <td class="pagination_next"><a href="<?php echo $this->request->variables['prefixedUrl'] ?>?page=<?php $this->e( $this->lastPage ) ?>">Dernier</a></td>
-        <?php endif ?>
-        </tr>
-    </tbody>
-</table>
+<?php if ( $this->lastPage > 1 ): ?>
+
+    <table class="pagination" align="center">
+        <tbody>
+            <tr>
+            <?php if ( $this->currentPage == 1 ): ?>
+            <?php else: ?>
+            <td class="pagination_previous"><a href="<?php echo $this->request->variables['prefixedUrl'] ?>?page=1">Premier</a>
+            <td class="pagination_previous"><a href="<?php echo $this->request->variables['prefixedUrl'] ?>?page=<?php $this->e( $this->currentPage - 1 ) ?>">Précédent</a></td>
+            <?php endif ?>
+
+            <?php for( $i=1; $i <= $this->lastPage; $i++ ): ?>
+            <td class="pagenumber"><a href="<?php echo $this->request->variables['prefixedUrl'] ?>?page=<?php $this->e( $i ) ?>"><?php $this->e( $i ) ?></a></td>
+            <?php endfor ?>
+
+            <?php if ( $this->currentPage == $this->lastPage ): ?>
+            <?php else: ?>
+            <td class="pagination_next"><a href="<?php echo $this->request->variables['prefixedUrl'] ?>?page=<?php $this->e( $this->currentPage + 1 ) ?>">Suivant</a></td>
+            <td class="pagination_next"><a href="<?php echo $this->request->variables['prefixedUrl'] ?>?page=<?php $this->e( $this->lastPage ) ?>">Dernier</a></td>
+            <?php endif ?>
+            </tr>
+        </tbody>
+    </table>
+
+<?php endif ?>
