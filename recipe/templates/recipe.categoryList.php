cae40d8ccd16b44fd10e59e137a5986e5c347425
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-06-11 13:49:21 +0000

    s/listByCategory/categoryDetails/
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1625 68bcc517-35a1-de11-95e8-003048d8a48a

--- recipe/templates/recipe.categoryList.php
+++ recipe/templates/recipe.categoryList.php
@@ -1,7 +1,7 @@
 <ul>
 <?php foreach( $this->objectList as $object ): ?>
     <li>
-        <a href="<?php $this->url( 'recipe.listByCategory', $object) ?>" title="<?php $this->e( $object['title'] ) ?>">
+        <a href="<?php $this->url( 'recipe.categoryDetails', $object) ?>" title="<?php $this->e( $object['title'] ) ?>">
             <?php $this->e( $object['title'] ) ?>
         </a>
     </li>
