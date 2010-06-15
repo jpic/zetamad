12545a1aed204e0a9851065a04f271af99faa8bb
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-06-15 18:15:08 +0000

    Fixed html h2 title
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1671 68bcc517-35a1-de11-95e8-003048d8a48a

--- recipe/templates/forms/recipe.recipe.php
+++ recipe/templates/forms/recipe.recipe.php
@@ -6,7 +6,7 @@
     </div>
     <?php endif ?>
 
-    <h2><?php echo ucfirst( $this->t( 'META.TITLE' ) ) ?></h2>
+    <h2><?php echo ucfirst( $this->metaTitle ) ?></h2>
 
     <fieldset class="inlineLabels">
         <h2>Présentation générale</h2>
