05498711b506592f67834fa5175ecd39cf80f6d4
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-06-25 11:56:07 +0000

    Command to dump our tables
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1765 68bcc517-35a1-de11-95e8-003048d8a48a

--- liria/bin/dump.sh
+++ liria/bin/dump.sh
@@ -6,7 +6,7 @@ for table in $tables; do
     sql="show tables where Tables_in_mad like '$table%'"
     subtables=$(echo "$sql" | mysql -u root -N mad)
 
-    for subtable in table; do
-        mysqldump -u root mad $table
+    for subtable in $subtables; do
+        mysqldump -u root mad $subtable
     done
 done
