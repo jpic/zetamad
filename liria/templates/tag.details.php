0c8d925b5fb7eeae7a9b33fbc31d7aaedfbd2279
jpic <jpic@68bcc517-35a1-de11-95e8-003048d8a48a>
2010-06-25 13:55:15 +0000

    Themed tag details title
    git-svn-id: svn://francepremium.nfrance.com/madeleinemarket.com@1773 68bcc517-35a1-de11-95e8-003048d8a48a

--- liria/templates/tag.details.php
+++ liria/templates/tag.details.php
@@ -1,6 +1,9 @@
 <?php $this->includeOnceTemplate( 'recipe.header.php' ) ?>
 
-<h2>Recettes du tag <?php $this->e( $this->object['name'] ) ?></h2>
+<div style="clear: both; padding-top: 0;" class="manufacturer-title">
+    <h4>Recettes du tag <?php $this->e( $this->object['name'] ) ?></h4>
+    <div class="clear"></div>
+</div>
 
 <?php
 $this->includeTemplate( 'recipe.list.php' );
